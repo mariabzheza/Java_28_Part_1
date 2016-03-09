eb0117cc57305516a4c708f3b40a1dc2d2163b1b
mariabzheza <maria.bzheza@gmail.com>
2016-03-09 15:57:28 +0200

    optimized transitions between pages
    optimized transitions between pages

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -14,12 +14,18 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoGroupPage() {
-        //click(By.linkText("Групи"));
+        if ( isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new")) ) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void goToHomePage() {
-        //click(By.linkText("Головна"));
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         click(By.linkText("home"));
     }
 }
