6eea0e85d83f3411794697dce445182b27de4fce
mariabzheza <maria.bzheza@gmail.com>
2016-03-14 12:46:52 +0200

    add GroupCount Assert

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -11,13 +11,6 @@ public class Collections {
 
     public static void main(String[] args) {
 
-        String[] langs = {"Java", "C#", "Python", "PHP"};
-
-        for (int i = 0; i < langs.length; i++) {
-            System.out.println("I want to learn " + langs[i]);
-        }
-        System.out.println("***************************");
-
         List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
 
         for (String l : languages) {
