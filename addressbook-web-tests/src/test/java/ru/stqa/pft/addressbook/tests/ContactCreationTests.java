6e0cdd7ecdf2904128ee1a1166bd7710cd8f9a6d
mariabzheza <maria.bzheza@gmail.com>
2016-03-18 14:58:03 +0200

    add using Comparator without previous for into Contact and Group Creation Tests
    add using Comparator without previous for into Contact and Group
    Creation Tests.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -20,13 +21,17 @@ public class ContactCreationTests extends TestBase {
         //int after = app.getContactHelper().getContactCount();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() +1);
-
-        int max = 0;
+        /*int max = 0;
         for(ContactData c: after) {
             if (c.getId() > max) {
                 max = c.getId();
             }
         }
+        */
+        // use Comparator without previous for
+        Comparator<? super ContactData> byId = (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+        int max = after.stream().max(byId).get().getId();
+
         contact.setId(max);
         before.add(contact);
         Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
