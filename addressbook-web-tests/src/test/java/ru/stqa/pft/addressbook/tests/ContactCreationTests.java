85dcac33a23a330e00f9a57ac9175cdea5e29d45
mariabzheza <maria.bzheza@gmail.com>
2016-04-14 15:04:07 +0300

    Add read from csv file in DataProvider
    Add read from csv file in DataProvider

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -6,7 +6,7 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -19,10 +19,10 @@ public class ContactCreationTests extends TestBase {
 
     @DataProvider
     // Ітератор масивів обєктів
-    public Iterator<Object[]> validContacts() {
+    public Iterator<Object[]> validContacts() throws IOException {
         File photo = new File("src/test/resources/stru.png");
         List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[] {new ContactData()
+        /*list.add(new Object[] {new ContactData()
                 .withFirstName("firstname").withLastName("lastname")
                 .withNickName("nickname").withTitle("jobtitle").withCompany("company")
                 .withHomeAddress("homeaddress").withAddress2("address2")
@@ -30,16 +30,23 @@ public class ContactCreationTests extends TestBase {
                 .withPhone2("77-89").withFax("987")
                 .withEmail("email@e.com").withEmail2("email2@e.com").withEmail3("email3@e.com")
                 .withHomePage("homepage.com").withNotes("notes")
-                .withGroup("test1").withPhoto(photo)});
-        list.add(new Object[] {new ContactData()
-                .withFirstName("firstname22").withLastName("lastname")
-                .withNickName("nickname").withTitle("jobtitle").withCompany("company")
-                .withHomeAddress("homeaddress").withAddress2("address2")
-                .withHomePhone("22-33-15").withMobile("+7(34) 55").withWorkPhone("33-33")
-                .withPhone2("77-89").withFax("987")
-                .withEmail("email@e.com").withEmail2("email2@e.com").withEmail3("email3@e.com")
-                .withHomePage("homepage.com").withNotes("notes")
-                .withGroup("test1").withPhoto(photo)});
+                .withGroup("test1").withPhoto(photo)}); */
+
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new ContactData()
+                    .withFirstName(split[0]).withLastName(split[1])
+                    .withNickName(split[2]).withTitle(split[3]).withCompany(split[4])
+                    .withHomeAddress(split[5]).withAddress2(split[6])
+                    .withHomePhone(split[7]).withMobile(split[8]).withWorkPhone(split[9])
+                    .withPhone2(split[10]).withFax(split[11])
+                    .withEmail(split[12]).withEmail2(split[13]).withEmail3(split[14])
+                    .withHomePage(split[15]).withNotes(split[16])
+                    .withGroup(split[17]).withPhoto(photo)});
+            line= reader.readLine();
+        }
         return list.iterator();
     }
 
